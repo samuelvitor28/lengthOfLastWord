d9ab2915b5c5148722ec6ea9d92035cde6d0f0de
Samuel Vitor Silveira da Silva Silva <samuelvitor2802@gmail.com>
2026-04-17 15:33:41 -0300

    adicionando código no gitHub.

---
+++ src/Main.java
@@ -0,0 +1,24 @@
+public class Main {
+    public static void main(String[] args) {
+        System.out.println(lengthOfLastWord("teste teste"));
+
+    }
+    static int lengthOfLastWord(String s){
+        int length = 0;
+        boolean found = false;
+        char [] sChar = s.toCharArray();
+
+        for (int i = sChar.length - 1; i >= 0 && !found; i--){
+            if(sChar[i] != ' '){
+                length++;
+                if(i != 0){
+                    if(sChar[i - 1] == ' '){
+                        found = true;
+                    }
+                }
+            }
+        }
+
+        return length;
+    }
+}
