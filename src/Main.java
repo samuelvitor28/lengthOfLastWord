189bc18908d5d554bbbf94f7c4c1fdd18c0eaa0c
Samuel Vitor Silveira da Silva Silva <samuelvitor2802@gmail.com>
2026-04-17 15:37:13 -0300

    adicionando solução no github.

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,6 @@
 public class Main {
     public static void main(String[] args) {
+        // teste
         System.out.println(lengthOfLastWord("teste teste"));
 
     }
@@ -18,7 +19,6 @@ public class Main {
                 }
             }
         }
-
         return length;
     }
 }
